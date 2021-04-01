4f0a129f413d600a38c787524a77ac459b7ac4fa
Stanislaw Baranski <stan@stan.bar>
2021-04-02 01:58:15 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let acorn = require("acorn");
+const parsed = acorn.parse("function main(a, b) { c = a + b; return c }", {ecmaVersion: 2020})
+
+
+const {input, body} = extractInputsAndBody(parsed.body)
+flattenBody(iniput, body)
+
+
+function extractInputsAndBody(code) {
+  o = []
+  inputs = []
+
+  for (node of code) {
+    node.params.forEach(param => { inputs.push(param.name) })
+  }
+  console.log(inputs)
+
+  let returned = false
+
+  let body = []
+  let block = code[0].body
+  if (block.type != 'BlockStatement') {
+    throw new Error("Only block statement supported")
+  }
+  console.dir(block.body)
+  for (statement of block.body) {
+    if(statement.type != 'ExpressionStatement' && statement.type != 'ReturnStatement') {
+      throw new Error("Unsupported statement")
+    }
+    if(returned){
+      throw new Error("Return has to be the last expression")
+    }
+    if (statement.type == "ExpressionStatement") {
+      const {left, right, operator} = statement.expression
+      body.push(statement.expression)
+    }
+    if (statement.type == 'ReturnStatement') {
+      returned = true
+    }
+  }
+  return { inputs, body }
+}
+
+function flattenBody(body) {
+  let o = []
+  for (statement of body) {
+    o.push(flattenStatement(statement))
+  }
+  return o
+}
+
