c9819ca1256b1fcbc822b33a19676cda974a9870
Stanislaw Baranski <stan@stan.bar>
2021-04-03 00:14:06 +0200

    refactor: fix eslint airbnb problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-use-before-define': 0,
+    'no-console': 0,
+  },
 };
